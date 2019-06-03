59cc1fe453e1bb60efc392eb11f8659cd8aec2e8
Camila Fernandez <camila.fernandez@utec.edu.pe>
2019-06-03 10:30:17 -0500

    Complete

--- src/simplifying_method_calls/replace-parameter-with-explicit-methods_before.java
+++ src/simplifying_method_calls/replace-parameter-with-explicit-methods_before.java
@@ -1,15 +1,3 @@
-void setValue(String name, int value) {
-  if (name.equals("height")) {
-    height = value;
-    return;
-  }
-  if (name.equals("width")) {
-    width = value;
-    return;
-  }
-  Assert.shouldNeverReachHere();
-}
-
 void setHeight (int value){
     height = value;
     return;
